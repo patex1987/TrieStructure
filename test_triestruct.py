c3eb4f2b350723ce8b7d2ec134549c7c7bf03396
patex1987 <patex1987@home_PC>
2017-05-01 12:43:30 +0200

    Tests updated according to the new structure

--- test_triestruct.py
+++ test_triestruct.py
@@ -7,8 +7,8 @@ def test_insert_single():
     Test of the structure, when a single element is inserted
     """
     trie = TrieStruct.Trie()
-    trie.insert("1234")
-    assert len(trie.get_words()) == 1
+    trie.insert_multiple_words(trie, ["1234"])
+    assert len(trie.get_all_words()) == 1
 
 
 def test_insert_list():
@@ -16,28 +16,8 @@ def test_insert_list():
     Test of the structure, when a collection of elements is inserted
     """
     trie = TrieStruct.Trie()
-    trie.insert_multiple_words(["1234", "5678", "345287"])
-    assert len(trie.get_words()) == 3
-
-
-def test_insert_wrong_type():
-    """
-    Test of the structure, when an integer is inserted instead of a string
-    """
-    trie = TrieStruct.Trie()
-    with pytest.raises(TypeError):
-        trie.insert(1234)
-
-
-def test_insert_list_wrong_type():
-    """
-    Test of the structure, when an integer is inserted instead of a collection
-    of strings
-    """
-    trie = TrieStruct.Trie()
-    with pytest.raises(TypeError):
-        trie.insert_multiple_words(1234)
-        trie.insert_multiple_words([1234, 5678, 890])
+    trie.insert_multiple_words(trie, ["1234", "5678", "345287"])
+    assert len(trie.get_all_words()) == 3
 
 
 def test_get_words_with_prefixes():
@@ -45,7 +25,7 @@ def test_get_words_with_prefixes():
     Test of the get words with prefix method
     """
     trie = TrieStruct.Trie()
-    trie.insert_multiple_words(["0902123456", "0902123897", "09023452",
-                                "09026745362", "76453", "98763"])
-    prefix_words = trie.get_words_with_prefix("0902123")
+    trie.insert_multiple_words(trie, ["0902123456", "0902123897", "09023452",
+                                      "09026745362", "76453", "98763"])
+    prefix_words = trie.get_all_words_with_prefix("0902123")
     assert len(prefix_words) == 2
